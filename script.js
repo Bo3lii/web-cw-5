fcd12e48f550e2eefd64be819dde590dd4f81ef8
Bo3lii <aoosa.alhaddad1@gmail.com>
2022-03-16 20:04:28 +0300

    CW3D5

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.log("انا طالب في الصف العاشر" );
 console.log('%c CCCCCCCColor ', 'color: #f9c74f');
 
 //javascript is not java
+
+
+let badget = 20;
+
+console.log(badget>=30 && badget<=80)
+console.log(badget + 30 >=30 && badget +30<=80 )
+console.log(badget + 30 - 30>=30 && badget + 30 - 30<=80)
